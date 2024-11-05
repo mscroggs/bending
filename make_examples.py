e8143caf4b973f5729633706a1a6d38a85a8108e
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-11-05 10:52:22 +0000

    torus!

--- make_examples.py
+++ make_examples.py
@@ -1,7 +1,13 @@
-from bending import animate_cylinder, make_gif
+from bending import animate_cylinder, animate_torus, make_gif
 
+"""
 animate_cylinder("puffin.jpg", direction="horizontal", folder="puffin-h", output_width=500)
 make_gif("puffin-h", "examples/cylinder-h.gif")
 
 animate_cylinder("puffin.jpg", direction="vertical", folder="puffin-v", output_width=500)
 make_gif("puffin-v", "examples/cylinder-v.gif")
+"""
+
+animate_torus("puffin.jpg", folder="puffin-torus", output_width=500)
+make_gif("puffin-torus", "examples/torus.gif")
+
